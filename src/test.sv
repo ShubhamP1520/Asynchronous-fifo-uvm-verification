07ec961e9dc259409ae251057eb6989a04f21a04
ShubhamP1520 <shubhampotadar3@gmail.com>
2025-10-07 20:18:27 +0530

    Create test.sv

---
+++ src/test.sv
@@ -0,0 +1,22 @@
+class fifo_test extends uvm_test;
+  `uvm_component_utils(fifo_test)
+
+  fifo_vsequence vseq;
+  fifo_env env;
+
+  function new(string name = "fifo_test", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    env = fifo_env::type_id::create("env", this);
+  endfunction
+
+  task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    vseq = fifo_vsequence::type_id::create("vseq");
+    vseq.start(env.vseqr);
+    phase.drop_objection(this);
+  endtask
+endclass
